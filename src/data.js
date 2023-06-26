221c13d661bac8adc1651a38bc1725524c184c02
Nancy Nallely Martinez Dominguez <nancymartinezdominguez11@gmail.com>
2023-06-26 08:57:14 -0600

    Se dio funcionalidad js a select AZ

--- src/data.js
+++ src/data.js
@@ -1,11 +1,62 @@
-// estas funciones son de ejemplo
-
 const data = {
+    OrdenaPeliculasAz(arrayPeliculas) {
+        let peliculasOrdenadas = arrayPeliculas.sort((a, b) => {// Declaramos variable peliculas ordenadas igual al arreglopeliculas se aplica el metodo sort y se le pasa una funcion que contien los parametros a y b
+            return a.title.localeCompare(b.title);// regresa el resultado de comparar el titulo a con el titulo b, localeCompare ordena alfabeticamente en el ciclo
+
+        });
+        return peliculasOrdenadas;
+    },
+
+    OrdenaPeliculasZa(arrayPeliculas) {
+        let peliculasOrdenadasZa = arrayPeliculas.reverse((a, b) => {// Declaramos variable peliculas ordenadas igual al arreglopeliculas se aplica el metodo sort y se le pasa una funcion que contien los parametros a y b
+            return a.title.localeCompare(b.title);// regresa el resultado de comparar el titulo a con el titulo b, localeCompare ordena alfabeticamente en el ciclo
+
+        });
+        return peliculasOrdenadasZa;
+    },
+
+    OrdenaPeliculasAño(arrayPeliculas) {
+        let peliculasOrdenadasAño = arrayPeliculas.sort((a, b) => {// Declaramos variable peliculas ordenadas igual al arreglopeliculas se aplica el metodo sort y se le pasa una funcion que contien los parametros a y b
+            return b.release_date.localeCompare(a.release_date);// regresa el resultado de comparar el titulo a con el titulo b, localeCompare ordena alfabeticamente en el ciclo
+
+        });
+        return peliculasOrdenadasAño;
+    },
+
+    OrdenaPeliculasScore(arrayPeliculas) {
+        let peliculasOrdenadasScore = arrayPeliculas.sort((a, b) => {// Declaramos variable peliculas ordenadas igual al arreglopeliculas se aplica el metodo sort y se le pasa una funcion que contien los parametros a y b
+            return b.rt_score - a.rt_score;// regresa el resultado de comparar el titulo a con el titulo b, localeCompare ordena alfabeticamente en el ciclo
+
+        });
+        return peliculasOrdenadasScore;
+    },
+
+    OrdenaPeliculasBy(arrayPeliculas) {
+        return arrayPeliculas;
+
+    },
 
 
-};
+    OrdenaPeliculasProductor(arrayPeliculas, nombreProductor) {
+        const resultadoProductor = arrayPeliculas.filter(pelicula => pelicula.producer == nombreProductor);
+        if (nombreProductor == "Producers") {
+            return arrayPeliculas;
+        } else {
+            return resultadoProductor;
+        }
+    },
+
+    OrdenaPeliculasDirector(arrayPeliculas, nombreDirector) {
+        const resultadoDirector = arrayPeliculas.filter(pelicula => pelicula.director == nombreDirector);
+        if (nombreDirector == "Directors") {
+            return arrayPeliculas;
+        } else {
+            return resultadoDirector;
+        }
+    }
+}
 
 export default data;
- 
+
 
 
--- src/main.js
+++ src/main.js
@@ -12,13 +12,72 @@ function paginaPrincipal() {
     location.href = "index.html";// Se agrega a la funcion principal la localizacion para direccionar 
 }
 
+const selectElement = document.querySelector('#ordenaPor');//Se crea una constante y se le asigna el elemento que se obtiene con queryselector
+if (selectElement != null) {
+    selectElement.addEventListener('change', (event) => {// A la constante se le agrega un elemento, que cuando cambia crea un evento
+        const opcionSeleccionada = event.target.value;// Se declara una constante y le asigna el valor del elemento
+    
+        switch (opcionSeleccionada) {
+            case "1":
+                let peliculasAz;
+                peliculasAz = data.OrdenaPeliculasAz(peliculas.films);
+                mostrarPeliculas(peliculasAz);
+                break;
+            case "2":
+                let peliculasZa;
+                peliculasZa = data.OrdenaPeliculasZa(peliculas.films);
+                mostrarPeliculas(peliculasZa);
+                break;
+            case "3":
+                let peliculasAño;
+                peliculasAño = data.OrdenaPeliculasAño(peliculas.films);
+                mostrarPeliculas(peliculasAño);
+                break;
+            case "4":
+                let peliculasScore;
+                peliculasScore = data.OrdenaPeliculasScore(peliculas.films);
+                mostrarPeliculas(peliculasScore);
+                break;
+            case "0":
+                let peliculasBy;
+                peliculasBy = data.OrdenaPeliculasBy(peliculas.films);
+                mostrarPeliculas(peliculasBy);
+                break;
+            default:
+                break;
+        }
+    
+    });
+}
+
+const selectProductor = document.querySelector('#Producers');//Se crea una constante y se le asigna el elemento que se obtiene con queryselector
+if (selectProductor != null) {
+    selectProductor.addEventListener('change', (event) => {// A la constante se le agrega un elemento, que cuando cambia crea un evento
+        const opcionSeleccionada = event.target.value;// Se declara una constante y le asigna el valor del elemento
+        let peliculasProductor;
+        peliculasProductor = data.OrdenaPeliculasProductor(peliculas.films, opcionSeleccionada);
+        mostrarPeliculas(peliculasProductor);
+    });
+}
+
+
+const selectDirector = document.querySelector('#Directors');//Se crea una constante y se le asigna el elemento que se obtiene con queryselector
+if (selectDirector != null) {
+    selectDirector.addEventListener('change', (event) => {// A la constante se le agrega un elemento, que cuando cambia crea un evento
+        const opcionSeleccionada = event.target.value;// Se declara una constante y le asigna el valor del elemento
+        let peliculasDirector;
+        peliculasDirector = data.OrdenaPeliculasDirector(peliculas.films, opcionSeleccionada);
+        mostrarPeliculas(peliculasDirector);
+    });
+}
+
 function mostrarPeliculas(peliculas) {
     let carSeccion = document.querySelector(".cards");// Se usa querySelector para encontrar la primera coincidencia de cards
     let cards = "";
     let colorFondo = 1;
     peliculas.forEach(pelicula => {// Se recorre el array que contien las peliculas pormedio de forEach
         let card = '<article class="card">';// declara la variable card y se le asigna una etiqueta article como texto
-        card += '<picture class="thumbnail">';// a la variable card se le concatena etiqueta picture
+        card += '<picture class="imagen">';// a la variable card se le concatena etiqueta picture
         card += '<img class="category__' + colorFondo++ + '" src="' + pelicula.poster + '" alt="" />';
         card += '</picture>';
         card += '<div class="card-content">';
@@ -27,10 +86,10 @@ function mostrarPeliculas(peliculas) {
         card += '</div><!-- .card-content -->';
         card += '<footer>';
         card += '<div class="post-meta">';
-        card += '<span class="timestamp"><i class="fa fa-clock-o"></i>Score: ' + pelicula.rt_score + '</span>';
-        card += '<span class="comments"><i class="fa fa-comments"></i>Año: ' + pelicula.release_date + '</span>';
-        card += '<span class="comments"><i class="fa fa-comments"></i>Productor: ' + pelicula.producer + '</span>';
-        card += '<span class="comments"><i class="fa fa-comments"></i>Director: ' + pelicula.director + '</span>';
+        card += '<span class="timestamp"><i></i>Score: ' + pelicula.rt_score + '</span>';
+        card += '<span class="comments"><i></i>Año: ' + pelicula.release_date + '</span>';
+        card += '<span class="comments"><i>></i>Productor: ' + pelicula.producer + '</span>';
+        card += '<span class="comments"><i></i>Director: ' + pelicula.director + '</span>';
         card += '</div>';
         card += '</footer>';
         card += '</article>';
