6511d755710e43bdc003a34dee1cb6aa951b3743
Nancy Nallely Martinez Dominguez <nancymartinezdominguez11@gmail.com>
2023-07-03 22:50:07 -0600

    se corrigieron errores

--- src/main.js
+++ src/main.js
@@ -1,34 +1,6 @@
 import data from './data.js';
 import peliculas from './data/ghibli/ghibli.js';// Se importa la data de studio ghibli
 
-function mostrarPeliculas(peliculas) {
-  const carSeccion = document.querySelector(".cards");// Se usa querySelector para encontrar la primera coincidencia de cards
-  let cards = "";
-  let colorFondo = 1;
-  peliculas.forEach(pelicula => {// Se recorre el array que contien las peliculas pormedio de forEach
-    let card = '<article class="card">';// declara la variable card y se le asigna una etiqueta article como texto
-    card += '<picture class="imagen">';// a la variable card se le concatena etiqueta picture
-    card += '<img class="category__' + colorFondo++ + '" src="' + pelicula.poster + '" alt="" />';
-    card += '</picture>';
-    card += '<div class="card-content">';
-    card += '<p class="category category__' + colorFondo + '">' + pelicula.title + '</p><br><br>';
-    card += '<p>' + pelicula.description + '</p>';
-    card += '</div><!-- .card-content -->';
-    card += '<footer>';
-    card += '<div class="post-meta">';
-    card += '<span class="timestamp"><i></i>Score: ' + pelicula.rt_score + '</span>';
-    card += '<span class="comments"><i></i>Año: ' + pelicula.release_date + '</span>';
-    card += '<span class="comments"><i>></i>Productor: ' + pelicula.producer + '</span>';
-    card += '<span class="comments"><i></i>Director: ' + pelicula.director + '</span>';
-    card += '</div>';
-    card += '</footer>';
-    card += '</article>';
-    //console.log("esto hace el forEach en card: "+card);
-    cards += card;
-  });
-  carSeccion.innerHTML = cards;
-}
-
 const botonVolverPrincipal = document.getElementById("principal");// Se declara la variable botonVolverPrincipal, y se llama al elemento del DOM por su Id.
 if (botonVolverPrincipal !== null) {
   botonVolverPrincipal.addEventListener("click", data.paginaPrincipal);// Se crea un evento al dar click se dirige a la funcion pagina principal
